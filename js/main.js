ca7ebb73b7127826fdc599818f9c4b58a7670988
CarloCassini <c.cassini87@gmail.com>
2023-08-23 16:10:13 +0200

    calcolo la deadline del giorno di domani

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 console.log("js ok");
+
+// millisecondi in un giorno == 86400000
+
+// Creare una data di base
+let timeNow = new Date();
+let deadline = new Date();
+
+// imposto la data di deadline;
+deadline.setDate(timeNow.getDate() + 1);
+deadline.setHours(9);
+deadline.setMinutes(30);
+deadline.setSeconds(0);
+deadline.setMilliseconds(0);
+
+console.log(deadline);
